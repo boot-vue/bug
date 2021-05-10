aeefc28c1476d5d07bcb5acb8ebbc9e51d84f156
bootvue <bootvue@gmail.com>
2021-05-10 01:41:51 +0000

    test

--- vite1/src/App.vue
+++ vite1/src/App.vue
@@ -1,27 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <span>Copyright © 2019 - {{ year }} vbeats</span>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent, reactive} from 'vue'
+import dayjs from "dayjs"
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "CopyRight",
+  setup() {
+    const data = reactive({
+      year: dayjs().year()
+    })
+
+    return {
+      ...data
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
