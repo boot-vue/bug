3d90a9035035cf496eaf9ffaa893b442550f8767
bootvue <bootvue@gmail.com>
2021-05-11 10:38:21 +0000

    test

--- vite1/src/App.vue
+++ vite1/src/App.vue
@@ -3,21 +3,24 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive} from 'vue'
-import dayjs from "dayjs"
+import { defineComponent, reactive } from "vue";
+import dayjs from "dayjs";
+import { message } from "ant-design-vue";
 
 export default defineComponent({
   name: "CopyRight",
   setup() {
     const data = reactive({
-      year: dayjs().year()
-    })
+      year: dayjs().year(),
+    });
+
+    message.success("xxxxxxxxxxxxxxxxxxxxxxxx", 0);
 
     return {
-      ...data
-    }
-  }
-})
+      ...data,
+    };
+  },
+});
 </script>
 
 <style>
