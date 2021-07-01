bb7f6cf3f2f7124391c4c9d9e55e0cc7ea422c7b
bootvue <bootvue@gmail.com>
2021-07-01 17:54:52 +0800

    ....

---
+++ tui_demo/src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <nut-overlay v-model:visible="show" :z-index="2000">
+      <div class="wrapper">
+        <div class="content">这里是正文</div>
+      </div>
+    </nut-overlay>
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import "./index.styl";
+
+export default {
+  setup() {
+    const show = true;
+    return {
+      show,
+    };
+  },
+};
+</script>
